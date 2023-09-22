c84184c2ef64f85fd83b1b1fe64764a0b883790b
adarshswamianth <adarshswamianth7@gmail.com>
2023-09-22 22:42:33 +0530

    [bug] second view of detils

--- src/Components/Batches/Details.jsx
+++ src/Components/Batches/Details.jsx
@@ -1,33 +1,66 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
-import { batchDetils } from '../../constants';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { batchDetils } from "../../constants";
 
-const Card = ({props}) => {
-  const {name,mentor,isCompleted,totalStudents,income} = props
-  return(
-    <div>
-      <h2>{name}</h2>
-      <h2>{mentor}</h2>
-      <h2>{isCompleted}</h2>
-      <h2>{totalStudents}</h2>
-      <h2>{income}</h2>
-
+// @dev display the student detils section
+const StudentDetils = ({ props }) => {
+  const { name, batch, fee, contactNumber } = props;
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4 mt-4">
+      <h1 className="text-xl font-semibold mb-2">{name}</h1>
+      <div className="flex flex-col md:flex-row justify-between mb-2">
+        <p className="text-gray-600">Batch: {batch}</p>
+        <p className="text-gray-600 md:ml-4">Fee: ${fee}</p>
+      </div>
+      <p className="text-gray-600">Contact Number: {contactNumber}</p>
     </div>
-  )
-}
+  );
+};
 
-
-function Details() {
-  const location = useLocation();
-  const name = location.state
-  console.log(name)
+// @dev section to display about the batch
+const Box = ({ props }) => {
+  const { name, mentor, isCompleted, totalStudents, income, students } = props;
+  console.log(students);
   return (
     <div>
-      {batchDetils.map((value) => (
-        value.name === name && <Card props={value}/>
-      ))}
+      <div className="bg-white rounded-lg shadow-lg p-6 mx-auto my-4 w-72">
+        <p className="text-2xl font-bold">Name: {name}</p>
+        <p className="mt-2 text-gray-600">Mentor: {mentor}</p>
+        <p className="mt-2">
+          {isCompleted ? (
+            <span className="text-green-500 font-bold">Completed</span>
+          ) : (
+            <span className="text-red-500 font-bold">Not Completed</span>
+          )}
+        </p>
+        <p className="mt-2 text-gray-600">Total Students: {totalStudents}</p>
+        <p className="mt-2 text-gray-600">Total Income: {income}</p>
+      </div>
+
+      <h3 className="text-center text-2xl mt-3 font-semibold underline decoration-green-500">
+        Student detils
+      </h3>
+      <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        {students.map((student) => (
+          <StudentDetils key={student.contactNumber} props={student} />
+        ))}
+      </div>
     </div>
-  )
+  );
+};
+// @dev main component
+function Details() {
+  const location = useLocation();
+  const name = location.state;
+  const batch = batchDetils.find((value) => value.name === name)
+  if(!batch){
+    return <div>No matching batch found.</div>
+  }
+  return (
+    <div className="p-3">
+      <Box props={batch}/>
+    </div>
+  );batches
 }
 
-export default Details
+export default Details;
